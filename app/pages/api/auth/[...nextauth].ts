adb0101376fc79adcdd1ab6f44c2d8baae6d5c06
Simon <nikhilsimontoppo@gmail.com>
2023-10-24 16:34:59 +0530

    signIn finished

--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import bcrypt from "bcrypt";
-import compare from "bcrypt";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
